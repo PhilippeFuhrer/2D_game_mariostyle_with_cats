eb7cf972b08a5b253a6563744280d9226208be83
PhilippeFuhrer <philippe.fuhrer@hotmail.com>
2022-06-07 15:38:03 +0200

    final_game stage 1

--- Main_class.py
+++ Main_class.py
@@ -3,6 +3,12 @@ from sys import exit
 from random import randint, choice
 
 
+#global variables
+score = 0
+score_shooting = 0
+passed_time = 0
+started_time = 0
+
 class Munition (pygame.sprite.Sprite):
     def __init__(self,player_pos_y):
         super().__init__()
@@ -19,7 +25,7 @@ class Munition (pygame.sprite.Sprite):
     def bullet_movement(self):
         if self.bullet_state == True:
             self.rect.x += 10
-        if self.rect.x >= 820:
+        if self.rect.x >= 800:
             self.bullet_state = False
 
     def destroy(self):
@@ -29,7 +35,7 @@ class Munition (pygame.sprite.Sprite):
     def update(self):
         self.shoot_bullet()
         self.bullet_movement()
-        #self.kill()
+        self.destroy()
 
 class Player (pygame.sprite.Sprite):
     def __init__(self):
@@ -44,11 +50,24 @@ class Player (pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = (80,266))
         self.gravity = 0
         self.double_jump = True
+        self.jump_sound_possible = True
 
+        self.jump_sound = pygame.mixer.Sound('Audio/jump.mp3')
+        self.jump_sound.set_volume(0.2)
+        
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.double_jump == True:
             self.gravity = -10
+        
+            if self.jump_sound_possible:
+                self.jump_sound.play()
+                self.jump_sound_possible = False
+            
+            if self.rect.y <= 180:
+                self.jump_sound.play()
+                self.jump_sound_possible = False
+
             
     def apply_gravity(self):
         self.gravity += 1 
@@ -56,9 +75,10 @@ class Player (pygame.sprite.Sprite):
         if self.rect.y >= 266:
             self.rect.y = 266
             self.double_jump = True
+            self.jump_sound_possible = True
         if self.rect.y < 50:
             self.double_jump = False
-
+        
         # player position for the bullet shooting
         global player_pos_y
         player_pos_y = self.rect.y
@@ -85,13 +105,16 @@ class Obstacle (pygame.sprite.Sprite):
             fly_1 = pygame.image.load('Graphics/fly1.png').convert_alpha()
             fly_2 = pygame.image.load('Graphics/fly2.png').convert_alpha()
             self.frames = [fly_1, fly_2]
-            y_pos = 210
+            list_y_pos = [230, 150, 100, 50]
+            y_pos = choice(list_y_pos)
+            self.type = "fly"
         
         else:
             snail_1 = pygame.image.load('Graphics/snail.png').convert_alpha()
             snail_2 = pygame.image.load('Graphics/snail2.png').convert_alpha()
             self.frames= [snail_1, snail_2]
             y_pos = 300
+            self.type = "snail"
 
         self.animation_index = 0
         self.image = self.frames[self.animation_index]
@@ -112,14 +135,36 @@ class Obstacle (pygame.sprite.Sprite):
         if self.rect.x <= -100:
             self.kill()
 
-
-def display_score():
-    current_time = int((pygame.time.get_ticks() - start_time)/10)
-    score_surf = test_font.render('Score: ' + f'{current_time}', False, "White")
-    score_rect = score_surf.get_rect(topleft = (40,40))
+def display_score(score_shooting):
+    global score
+    score += score_shooting
+    level = 1
+    global passed_time
+    if passed_time > 10000:
+        level = 2
+    if passed_time > 20000:
+        level = 3
+    score_surf = test_font.render('Score: ' + f'{score}', False, "White")   
+    score_rect = score_surf.get_rect(topleft = (40,30))
+    level_surf = test_font.render('Level: ' + f'{level}', False, "White")
+    level_rect = level_surf.get_rect(topleft = (650,30))
     screen.blit(score_surf, score_rect)
-    return current_time
+    screen.blit(level_surf, level_rect)
 
+    if passed_time >= 31000:
+        wave = "Final battle!"
+        wave_surf = test_font.render(wave, False, "White")
+        wave_rect = wave_surf.get_rect(topleft = (220, 30))
+        screen.blit(wave_surf, wave_rect)
+    if passed_time >= 100000:
+        wave2 = "Concrats! You won!"
+        wave2_surf = test_font.render(wave2, False, "White")
+        wave2_rect = wave2_surf.get_rect(topleft = (220, 30))
+        screen.blit(wave2_surf, wave2_rect)
+
+
+    return score
+    
 # end game if collision with obstacle
 def collision_sprite():
     if pygame.sprite.spritecollide(player.sprite, obstacle_group,True):
@@ -130,15 +175,17 @@ def collision_sprite():
 
 # if shooting obstacle, kill obstacle
 def collision_sprite_munition():
-    pygame.sprite.groupcollide(munition_group, obstacle_group,True, True)
-
+    if pygame.sprite.groupcollide(munition_group, obstacle_group,True, True):
+        explosion_sound.play()
+        global score_shooting
+        score_shooting += 100
+        
 #initial statements and settings for game
 pygame.init()
 game_running = True
 game_started = False
 game_end = False
-start_time = 0
-score = 0
+game_difficulty = 900
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Cat game <3')
 clock = pygame.time.Clock()
@@ -172,8 +219,22 @@ start_rect = start_text.get_rect(center = (-500, 50))
 obstacle_timer = pygame.USEREVENT +1
 pygame.time.set_timer(obstacle_timer, 900)
 
+obstacle_timer2 = pygame.USEREVENT +2
+pygame.time.set_timer(obstacle_timer2, 600)
+
+obstacle_timer3 = pygame.USEREVENT +3
+pygame.time.set_timer(obstacle_timer3, 300)
+
+obstacle_timer4 = pygame.USEREVENT +4
+pygame.time.set_timer(obstacle_timer4, 100)
+
+# sounds
+explosion_sound = pygame.mixer.Sound('Audio/explosion.wav')
+explosion_sound.set_volume(0.2)
+
 #Eventhandler
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -183,15 +244,26 @@ while True:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game_running = True
-                    start_time = pygame.time.get_ticks()
+                    started_time = pygame.time.get_ticks()
 
         if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     game_started = True
 
         if game_started == True and game_running == True:
-            if event.type == obstacle_timer:
-                obstacle_group.add(Obstacle(choice(['fly', 'snail', 'snail'])))
+            passed_time = pygame.time.get_ticks() - started_time
+            if passed_time > 100000:
+                pass
+            elif passed_time > 40000 and event.type == obstacle_timer4:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail'])))
+            elif passed_time > 30000:
+                pass
+            elif passed_time > 20000 and event.type == obstacle_timer3:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail'])))
+            elif passed_time > 10000 and event.type == obstacle_timer2:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail'])))
+            elif passed_time < 10000 and event.type == obstacle_timer:
+                obstacle_group.add(Obstacle(choice(['fly', 'snail'])))
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     munition_group.add(Munition(player_pos_y))
@@ -202,7 +274,9 @@ while True:
                         game_end = False
                         game_running = True
                         game_started = False
+                        passed_time = 0
     
+    #intro
     if game_running == True:
         screen.blit(start_surf, start_cat_rectangle)
         screen.blit(start_text, start_rect)
@@ -211,13 +285,16 @@ while True:
         start_rect.x += 4
         if start_rect.x > 800:
             start_rect.x = -600
-
+        score = 0
+        level = 1
+     
+    #gamemode
     if game_started == True:
         if game_running == True:
             pygame.draw.line(screen, "Black", (0,350), (800,350), width = 3)
             screen.blit(sky_surf,(0,0))
             screen.blit(ground_surf,(0,350))
-            score = display_score()
+            score = display_score(score_shooting)
             player.draw(screen)
             player.update()
             obstacle_group.draw(screen)
@@ -226,22 +303,20 @@ while True:
             munition_group.update()
             game_running = collision_sprite()
             collision_sprite_munition()
-
         else:
             pygame.draw.rect(screen, "Black", pygame.Rect(0, 0, 800, 400))
             screen.blit(endtext_surf, endtext_rect)
             screen.blit(endimg_surf_scaled, endimg_rect)
             player_gravity = 0
+            score_shooting = 0
 
             #display score
             score_message = test_font.render('Your Score: ' + f'{score}', False, "White")
             score_rect = score_message.get_rect(center = (400, 50))
-
             if score > 0:
                 screen.blit(score_message, score_rect)
             game_end = True
-
-
+            
     pygame.display.update()
     clock.tick(60)
 
